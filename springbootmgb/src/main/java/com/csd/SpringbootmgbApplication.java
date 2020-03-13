bb46a040df042d432b2c6b53f65d0f5de54c662f
csd <csd@qq.com>
2020-03-13 09:31:51 +0800

    项目结束第二次push

--- springbootmgb/src/main/java/com/csd/SpringbootmgbApplication.java
+++ springbootmgb/src/main/java/com/csd/SpringbootmgbApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringbootmgbApplication  {
     public static void main(String[] args) {
+        System.out.println("csd2020年3月13日09:30:30");
         SpringApplication.run(SpringbootmgbApplication.class, args);
     }
 }
